eaf588aabccc65f1c3777dd121df59fd7f7e9bf1
horstsal <161054666+horstsal@users.noreply.github.com>
2024-04-19 14:58:02 +0200

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -9,27 +9,22 @@ PYTHONANYWHERE_WEBAPPNAME = "mysite"
 app = Flask(__name__)
 
 my_type_role = """
-    As a digital therapy coach, check in daily with your patient to assess their well-being related to their chronic condition.
-    Use open-ended questions and empathetic dialogue to create a supportive environment.
-    Reflectively listen and encourage elaboration to assess the patient's detailed condition without directing the topic.
+Stellen Sie sich vor, Sie sind ein aufmerksamer Coach, der seinem Gegenüber hilft, sich an wichtige Details zu erinnern. Stellen Sie gezielte Fragen, um das Gedächtnis des Benutzers anzuregen, und fördern Sie einen dynamischen Austausch. Gehen Sie auf die Antworten des Nutzers ein und stellen Sie präzisere Folgefragen, um ein tieferes Verständnis zu erreichen.
 """
 
 my_instance_context = """
-    Meet Daniel Müller, 52, who is tackling obesity with a therapy plan that includes morning-to-noon intermittent fasting, 
-    thrice-weekly 30-minute swims, and a switch to whole grain bread.
+Stellen Sie ausschließlich geschlossene Fragen an den Benutzer, um präzise Antworten zu erhalten.
 """
 
 my_instance_starter = """
-Jetzt, frage nach dem Namen und einem persönlichen Detail (z.B. Hobby, Beruf, Lebenserfahrung).
-Verwende diese im geschlechtsneutralem Gespräch in Du-Form.
-Sobald ein Name und persönliches Detail bekannt ist, zeige eine Liste von Optionen.
+Heissen sie den Benutzer herzlich willkommen und eröffnen Sie die Gesprächsinteraktion freundlich und interessiert, indem Sie nachfragen, woran sich der Benutzer erinnern möchte. 
 """
 
 bot = Chatbot(
     database_file="database/chatbot.db", 
     type_id="coach",
     user_id="daniel",
-    type_name="Health Coach",
+    type_name="Erinnerungs-Coach B",
     type_role=my_type_role,
     instance_context=my_instance_context,
     instance_starter=my_instance_starter
